92371a9e30962db90e0e8761324d59645aa1cc63
Alex Edwards <lensgeek92@gmail.com>
2020-05-24 16:32:16 +0100

    Layout + Testing

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -1,17 +1,38 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import { render } from '@testing-library/react'
 import Header from '../header'
-import {axe} from 'jest-axe'
+import { axe } from 'jest-axe'
+import * as Gatsby from 'gatsby'
 
-const siteTitle = `Hello World`
+const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery');
+useStaticQuery.mockImplementation(() => ({
+  contentfulSiteMetadata: {
+    title: 'Hello-Title'
+  },
+}));
+
+function mockLocation(pathname = "/") {
+  return { pathname }
+}
 
 test('It displays the correct title', () => {
-  const {getByText} = render(<Header siteTitle={siteTitle} />)
-  const title = getByText(siteTitle)
+  const { getByText } = render(<Header location={mockLocation()} />)
+  expect(getByText(/hello\-title/i))
+})
+
+test('It has a CTA', () => {
+  const { getByRole } = render(<Header location={mockLocation()} />)
+  const button = getByRole('button')
+  expect(button)
+})
+
+test('It can have a custom title', () => {
+  const { getByText } = render(<Header location={mockLocation("/hello")} siteTitle="Hello" />)
+  const title = getByText(/^alex edwards/i)
   expect(title).toBeInTheDocument()
 })
 
 test('Passes A11y Testing', async () => {
-  const {container} = render(<Header siteTitle={siteTitle} />)
+  const { container } = render(<Header location={mockLocation()} />)
   expect(await axe(container)).toHaveNoViolations()
 })
---
+++ src/components/__tests__/layout.test.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { axe } from 'jest-axe'
+import * as Gatsby from 'gatsby'
+import Layout from '../Layout'
+
+const useStaticQuery = jest.spyOn(Gatsby, 'useStaticQuery');
+useStaticQuery.mockImplementation(() => ({
+  contentfulSiteMetadata: {
+    title: 'Hello-Title'
+  },
+  "allContentfulPage": {
+    "edges": [
+      {
+        "node": {
+          "title": "About",
+          "meta": {
+            "slug": "about"
+          }
+        }
+      },
+      {
+        "node": {
+          "title": "Contact",
+          "meta": {
+            "slug": "contact"
+          }
+        }
+      },
+      {
+        "node": {
+          "title": "Homepage",
+          "meta": {}
+        }
+      }
+    ]
+  },
+  "allContentfulAuthorSocialLinks": {
+    "edges": [
+      {
+        "node": {
+          "devTo": "https://dev.to/_lexedwards",
+          "github": "https://github.com/lexedwards/",
+          "instagram": "https://instagram.com/lex_script",
+          "linkedin": "https://www.linkedin.com/in/lexedwards/",
+          "twitter": "https://twitter.com/_lexedwards"
+        }
+      }
+    ]
+  }
+}));
+
+const testData = "Testing Title"
+
+test('it renders Children', () => {
+  const { getByText } = render(
+    <Layout location={{ pathname: "/" }}>
+      <p>{testData}</p>
+    </Layout>
+  )
+  expect(getByText(testData)).toBeInTheDocument()
+})
